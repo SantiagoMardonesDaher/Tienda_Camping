3b2a576ebace6f0578b1f79730a6ac39a398325b
Francisco Javier <fbottazzini@uade.edu.ar>
2024-08-27 17:06:34 -0300

    Product: Implementaicon de entity y las capas

--- src/main/java/com/uade/tpo/demo/entity/Product.java
+++ src/main/java/com/uade/tpo/demo/entity/Product.java
@@ -6,20 +6,45 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Data
 @Entity
 public class Product {
+
+    public Product() {
+    }
+
+    public Product(String descpription, Float price, int stock) {
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column()
     private String description;
 
-    @OneToOne
+    @Column()
+    private Float price;
+
+    @Column
+    private int stock;
+
+    @ManyToOne
     @JoinColumn(name = "category_id", referencedColumnName = "id")
     private Category category;
+
+    @OneToOne
+    private ProductOrder productOrder;
+
+    // @OneToMany(mappedBy = "product")
+    // private OrderItems OrderItems;
+
 }
