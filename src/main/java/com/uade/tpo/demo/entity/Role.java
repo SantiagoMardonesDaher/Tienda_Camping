2144d8a43b8fa84d5685ac5c83cebf8ad4007228
SantiagoMardonesDaher <smardones@uade.edu.ar>
2024-08-27 17:05:18 -0300

    capas de role y de orderItem

--- src/main/java/com/uade/tpo/demo/entity/Role.java
+++ src/main/java/com/uade/tpo/demo/entity/Role.java
@@ -8,9 +8,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToOne;
+import lombok.Data;
+
 
 @Entity
+@Data
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
