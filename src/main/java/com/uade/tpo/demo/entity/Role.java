f4c00af68f556c31c87ba97ce6f4b4a09ef5a3b8
SantiagoMardonesDaher <smardones@uade.edu.ar>
2024-09-03 13:34:23 -0300

    cambios role

--- src/main/java/com/uade/tpo/demo/entity/Role.java
+++ src/main/java/com/uade/tpo/demo/entity/Role.java
@@ -1,29 +1,6 @@
 package com.uade.tpo.demo.entity;
 
-import java.util.List;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import lombok.Data;
-
-
-@Entity
-@Data
-public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column
-    private String description;
-
-    @ManyToMany(mappedBy = "roles")
-    private List<User> users;
-
-    // @OneToOne
-    // private User user;
+public enum Role {
+    USER,
+    ADMIN
 }
