74644f11923bbc9dd8e5e6c3d24713f31001944e
SantiagoMardonesDaher <smardones@uade.edu.ar>
2024-09-03 14:53:19 -0300

    borrado

--- src/main/java/com/uade/tpo/demo/exceptions/RoleDuplicateException.java
+++
@@ -1,7 +0,0 @@
-package com.uade.tpo.demo.exceptions;
-
-public class RoleDuplicateException extends Exception {
-    public RoleDuplicateException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/uade/tpo/demo/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.uade.tpo.demo.service;
-
-import com.uade.tpo.demo.entity.Role;
-import java.util.List;
-
-public interface RoleService {
-    List<Role> getAllRoles();
-    Role getRoleByName(String name);
-}
