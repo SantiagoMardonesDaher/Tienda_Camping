636b644e6265abb9e1735b82cdb7d639ca63b480
SantiagoMardonesDaher <smardones@uade.edu.ar>
2024-09-04 16:44:15 -0300

    arreglo de entitys que no funcionaba

--- src/main/java/com/uade/tpo/demo/entity/OrderItem.java
+++ src/main/java/com/uade/tpo/demo/entity/OrderItem.java
@@ -1,25 +1,22 @@
 package com.uade.tpo.demo.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
 @Table(name = "order_items")
 public class OrderItem {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(nullable = false)
     private int quantity;
+
+    @Column(nullable = false)
+    private Float finalPrice;
 
     @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
@@ -28,7 +25,4 @@ public class OrderItem {
     @ManyToOne
     @JoinColumn(name = "order_id", nullable = false)
     private Order order;
-
-    @Column(name = "final_price", nullable = false)
-    private int finalPrice;
 }
--- src/main/java/com/uade/tpo/demo/entity/Product.java
+++ src/main/java/com/uade/tpo/demo/entity/Product.java
@@ -1,19 +1,10 @@
 package com.uade.tpo.demo.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-
-import jakarta.persistence.OneToOne;
-
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -24,7 +15,6 @@ public class Product {
     }
 
     public Product(String description, Float price, int stock) {
-
         this.description = description;
         this.price = price;
         this.stock = stock;
@@ -34,26 +24,21 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column()
+    @Column(nullable = false)
     private String description;
 
-    @Column()
+    @Column(nullable = false)
     private Float price;
 
-    @Column
+    @Column(nullable = false)
     private int stock;
 
     @ManyToOne
     @JoinColumn(name = "category_id", referencedColumnName = "id")
     private Category category;
 
-    @OneToOne
-    private Order productOrder;
-
-    @OneToMany(mappedBy = "product")
-    private OrderItem OrderItem;
-
-    @OneToMany
-    private Order order;
+    
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
 
 }
